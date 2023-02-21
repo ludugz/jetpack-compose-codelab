d62e608c636842b35f6770a4c34316fe25c96e21
Tan N. Truong <tntan92@gmail.com>
2023-02-21 23:56:15 +0900

    Update Greeting component UI

--- basic-codelab/app/src/main/java/com/example/codelab_1st_basic_compose/ui/component/Greeting.kt
+++ basic-codelab/app/src/main/java/com/example/codelab_1st_basic_compose/ui/component/Greeting.kt
@@ -1,6 +1,10 @@
 package com.example.codelab_1st_basic_compose.ui.component
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -12,12 +16,24 @@ import com.example.codelab_1st_basic_compose.ui.theme.BasicComposeTheme
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colorScheme.primary, modifier = Modifier.padding(24.dp)) {
-        Text(text = "Hello $name!")
+    Surface(color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier
+            .padding(vertical = 4.dp, horizontal = 8.dp)
+            .fillMaxWidth()) {
+        Row {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello!")
+                Text(text = name)
+            }
+
+            ElevatedButton(onClick = { /*TODO*/ }) {
+                Text("Show More")
+            }
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = false, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     BasicComposeTheme {
