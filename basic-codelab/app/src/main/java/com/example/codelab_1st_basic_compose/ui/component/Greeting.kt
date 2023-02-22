481c95a6f0a6e41735294d3bbfe1162c3f725a14
Tan N. Truong <tntan92@gmail.com>
2023-02-22 23:36:47 +0900

    Handle state & expand button in Greeting component

--- basic-codelab/app/src/main/java/com/example/codelab_1st_basic_compose/ui/component/Greeting.kt
+++ basic-codelab/app/src/main/java/com/example/codelab_1st_basic_compose/ui/component/Greeting.kt
@@ -9,6 +9,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -16,18 +18,34 @@ import com.example.codelab_1st_basic_compose.ui.theme.BasicComposeTheme
 
 @Composable
 fun Greeting(name: String) {
+    val expanded = remember {
+        mutableStateOf(false)
+    }
+
+    val expandedPadding = if (expanded.value) 48.dp else 0.dp
+
     Surface(color = MaterialTheme.colorScheme.primary,
         modifier = Modifier
             .padding(vertical = 4.dp, horizontal = 8.dp)
             .fillMaxWidth()) {
-        Row {
-            Column(modifier = Modifier.weight(1f)) {
+        Row(modifier = Modifier.padding(horizontal = 4.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f),
+            ) {
                 Text(text = "Hello!")
                 Text(text = name)
             }
 
-            ElevatedButton(onClick = { /*TODO*/ }) {
-                Text("Show More")
+            ElevatedButton(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = expandedPadding),
+                onClick = {
+                    expanded.value = !expanded.value
+                }
+            ) {
+                Text(if (expanded.value) "Show Less" else "Show More")
             }
         }
     }
