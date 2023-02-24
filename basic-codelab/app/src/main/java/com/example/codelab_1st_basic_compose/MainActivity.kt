2f30916e19aa4184a542b343f7140774f9ed8b7e
Tan N. Truong <tntan92@gmail.com>
2023-02-24 23:16:17 +0900

    Add animation & final touch to the UI

--- basic-codelab/app/src/main/java/com/example/codelab_1st_basic_compose/MainActivity.kt
+++ basic-codelab/app/src/main/java/com/example/codelab_1st_basic_compose/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.codelab_1st_basic_compose
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.codelab_1st_basic_compose.ui.OnboardingScreen
@@ -24,8 +26,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
-    val list = List(1000) { it.toString()}
-    var shouldShowOnboardingScreen by remember {
+    val list = List(100) { it.toString()}
+    var shouldShowOnboardingScreen by rememberSaveable {
         mutableStateOf(true)
     }
 
@@ -38,7 +40,12 @@ fun MyApp(modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = false, widthDp = 320)
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
 @Composable
 fun MyAppPreview() {
     BasicComposeTheme {
--- basic-codelab/app/src/main/java/com/example/codelab_1st_basic_compose/ui/component/Greeting.kt
+++ basic-codelab/app/src/main/java/com/example/codelab_1st_basic_compose/ui/component/Greeting.kt
@@ -1,25 +1,29 @@
 package com.example.codelab_1st_basic_compose.ui.component
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.codelab_1st_basic_compose.R
 import com.example.codelab_1st_basic_compose.ui.theme.BasicComposeTheme
 
 @Composable
@@ -28,19 +32,14 @@ fun Greeting(name: String) {
         mutableStateOf(false)
     }
 
-    val expandedPadding = animateDpAsState(
-        targetValue = if (expanded.value) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessMedium
-        )
-    )
-
     Surface(color = MaterialTheme.colorScheme.primary,
         modifier = Modifier
             .padding(vertical = 4.dp, horizontal = 8.dp)
             .fillMaxWidth()) {
-        Row(modifier = Modifier.padding(horizontal = 4.dp)) {
+        Row(modifier = Modifier
+            .padding(horizontal = 4.dp)
+            .animateContentSize(animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy,
+                stiffness = Spring.StiffnessMedium))) {
             Column(
                 modifier = Modifier
                     .weight(1f),
@@ -51,17 +50,29 @@ fun Greeting(name: String) {
                     style = MaterialTheme.typography.headlineMedium,
                     fontWeight = FontWeight.ExtraBold
                 )
+
+                AnimatedVisibility(
+                    visible = expanded.value,
+                    enter = fadeIn(animationSpec = spring(stiffness = Spring.StiffnessHigh)),
+                    exit = fadeOut(animationSpec = spring(stiffness = Spring.StiffnessHigh)),
+                ) {
+                    Text(text = "I'm reading a book called Atomic Habits and it's interesting".repeat(
+                        4))
+                }
             }
 
-            ElevatedButton(
+            IconButton(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = expandedPadding.value.coerceAtLeast(0.dp)),
+                    .padding(bottom = if (expanded.value) 48.dp else 0.dp),
                 onClick = {
                     expanded.value = !expanded.value
-                }
+                },
             ) {
-                Text(if (expanded.value) "Show Less" else "Show More")
+                Icon(imageVector = if (expanded.value) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = if (expanded.value) stringResource(id = R.string.show_less) else stringResource(
+                        id = R.string.show_more)
+                )
             }
         }
     }
