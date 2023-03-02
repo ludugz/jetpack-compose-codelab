046239c439f853b347d4d7940369f98e0a0498d3
Tan N. Truong <tntan92@gmail.com>
2023-03-02 22:14:59 +0900

    Add Button to WarterCounter

--- state-compose-codelab/app/src/main/java/ludugz/statecomposecodelab/ui/components/WaterCounter.kt
+++ state-compose-codelab/app/src/main/java/ludugz/statecomposecodelab/ui/components/WaterCounter.kt
@@ -1,22 +1,38 @@
 package ludugz.statecomposecodelab.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-   val count = 0
-    Text(
-        text = "You've had $count glasses",
-        modifier = Modifier.padding(16.dp),
-    )
+    val count = 0
+    Column {
+        Text(
+            text = "You've had $count glasses",
+            modifier = Modifier
+                .padding(16.dp),
+        )
+
+        OutlinedButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.background(color = Color.Gray),
+        ) {
+            Text(
+                text = "Add One",
+            )
+        }
+    }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun WaterCounterPreview() {
     WaterCounter()
