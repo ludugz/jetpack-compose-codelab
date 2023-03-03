8b64fd9e3f0426e61ae71db5e28624605e50dbcf
Tan N. Truong <tntan92@gmail.com>
2023-03-03 23:18:04 +0900

    Update state for Text & Button visibility

--- state-compose-codelab/app/src/main/java/ludugz/statecomposecodelab/ui/components/WaterCounter.kt
+++ state-compose-codelab/app/src/main/java/ludugz/statecomposecodelab/ui/components/WaterCounter.kt
@@ -5,7 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -13,17 +13,22 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
-    Column {
-        Text(
-            text = "You've had $count glasses",
-            modifier = Modifier
-                .padding(16.dp),
-        )
+    var count by remember {
+        mutableStateOf(value = 0)
+    }
 
+    Column {
+        if (count > 0) {
+            Text(
+                text = "You've had $count glasses",
+                modifier = Modifier
+                    .padding(16.dp),
+            )
+        }
         OutlinedButton(
-            onClick = { /*TODO*/ },
+            onClick = { count++ },
             modifier = Modifier.background(color = Color.Gray),
+            enabled = count < 10
         ) {
             Text(
                 text = "Add One",
