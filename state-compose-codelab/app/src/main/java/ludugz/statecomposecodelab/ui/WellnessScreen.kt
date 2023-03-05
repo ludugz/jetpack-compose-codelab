7ad75cfe8583e169d6d6442bd734bb86423e194b
Tan N. Truong <tntan92@gmail.com>
2023-03-05 22:34:17 +0900

    Apply state hoisting to counter

--- state-compose-codelab/app/src/main/java/ludugz/statecomposecodelab/ui/WellnessScreen.kt
+++ state-compose-codelab/app/src/main/java/ludugz/statecomposecodelab/ui/WellnessScreen.kt
@@ -3,11 +3,11 @@ package ludugz.statecomposecodelab.ui
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import ludugz.statecomposecodelab.ui.components.WaterCounter
+import ludugz.statecomposecodelab.ui.components.StatefulCounter
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier = modifier)
+    StatefulCounter(modifier = modifier)
 }
 
 @Preview(showBackground = true)
--- state-compose-codelab/app/src/main/java/ludugz/statecomposecodelab/ui/components/WaterCounter.kt
+++ state-compose-codelab/app/src/main/java/ludugz/statecomposecodelab/ui/components/WaterCounter.kt
@@ -1,60 +1,48 @@
 package ludugz.statecomposecodelab.ui.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
-    Column(modifier = Modifier.fillMaxWidth()) {
-        var count by remember {
-            mutableStateOf(value = 0)
-        }
+fun StatelessCounter(
+    modifier: Modifier = Modifier,
+    count: Int,
+    onValueChange: () -> Unit,
+) {
+    Column(modifier = modifier.padding(16.dp)) {
         if (count > 0) {
-            var enableTask by remember {
-                mutableStateOf(true)
-            }
-            if (enableTask) {
-                WellnessTaskItem(taskName = "Have you taken your 15 min walk today?") {
-                    enableTask = false
-                }
-            }
-            Text(text = "You have $count glasses")
+            Text("You've had $count glasses.")
         }
-        Row(modifier = Modifier.padding(8.dp)) {
-            OutlinedButton(
-                onClick = { count++ },
-                modifier = Modifier.background(color = Color.Gray),
-                enabled = count < 10
-            ) {
-                Text(
-                    text = "Add One",
-                )
-            }
-            OutlinedButton(
-                onClick = { count = 0 },
-                modifier = Modifier.background(color = Color.Gray),
-                enabled = count < 10
-            ) {
-                Text(
-                    text = "Clear",
-                )
-            }
+        OutlinedButton(
+            onClick = onValueChange,
+            Modifier.padding(top = 8.dp),
+            enabled = count < 10
+        ) {
+            Text("Add one")
         }
     }
 }
 
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by remember {
+        mutableStateOf(0)
+    }
+    StatelessCounter(count = count) {
+        count++
+    }
+
+}
+
 @Preview(showBackground = true)
 @Composable
-fun WaterCounterPreview() {
-    WaterCounter()
+fun StatefulCounterPreview() {
+    StatefulCounter()
 }
+
