de5de01fd23e13271be0a617e74655dc7edffa42
Tan N. Truong <tntan92@gmail.com>
2023-03-07 23:28:57 +0900

    Hoist task list on Screen

--- state-compose-codelab/app/src/main/java/ludugz/statecomposecodelab/ui/WellnessScreen.kt
+++ state-compose-codelab/app/src/main/java/ludugz/statecomposecodelab/ui/WellnessScreen.kt
@@ -2,8 +2,12 @@ package ludugz.statecomposecodelab.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import ludugz.statecomposecodelab.model.WellnessTask
+import ludugz.statecomposecodelab.model.getWellnessTasks
 import ludugz.statecomposecodelab.ui.components.StatefulCounter
 import ludugz.statecomposecodelab.ui.components.WellnessTaskList
 
@@ -11,7 +15,16 @@ import ludugz.statecomposecodelab.ui.components.WellnessTaskList
 fun WellnessScreen(modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
         StatefulCounter(modifier = modifier)
-        WellnessTaskList()
+
+        val list = remember {
+            mutableStateListOf<WellnessTask>().apply {
+                addAll(getWellnessTasks())
+            }
+        }
+
+        WellnessTaskList(taskList = list) { item ->
+            list.remove(item)
+        }
     }
 }
 
--- state-compose-codelab/app/src/main/java/ludugz/statecomposecodelab/ui/components/WellnessTaskList.kt
+++ state-compose-codelab/app/src/main/java/ludugz/statecomposecodelab/ui/components/WellnessTaskList.kt
@@ -3,24 +3,18 @@ package ludugz.statecomposecodelab.ui.components
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import ludugz.statecomposecodelab.model.WellnessTask
-import ludugz.statecomposecodelab.model.getWellnessTasks
 
 @Composable
-fun WellnessTaskList(modifier: Modifier = Modifier) {
-    val list = remember {
-        mutableStateListOf<WellnessTask>().apply {
-            addAll(getWellnessTasks())
-        }
-    }
+fun WellnessTaskList(
+    modifier: Modifier = Modifier,
+    taskList: List<WellnessTask>,
+    onItemClose: (WellnessTask) -> Unit,
+) {
     LazyColumn {
-        items(list) { item ->
-            WellnessTaskItem(taskName = item.label) {
-                list.remove(item)
-            }
+        items(taskList) { item ->
+            WellnessTaskItem(taskName = item.label, onClose = { onItemClose(item) })
         }
     }
 }
