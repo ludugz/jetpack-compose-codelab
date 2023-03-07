61d9c4abc290c566af84d8c2732226ce5f1f6a41
Tan N. Truong <tntan92@gmail.com>
2023-03-07 23:11:52 +0900

    Update onClose & onCheckedChange listener on WellnessScreen.kt

--- state-compose-codelab/app/src/main/java/ludugz/statecomposecodelab/ui/WellnessScreen.kt
+++ state-compose-codelab/app/src/main/java/ludugz/statecomposecodelab/ui/WellnessScreen.kt
@@ -1,17 +1,22 @@
 package ludugz.statecomposecodelab.ui
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ludugz.statecomposecodelab.ui.components.StatefulCounter
+import ludugz.statecomposecodelab.ui.components.WellnessTaskList
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    StatefulCounter(modifier = modifier)
+    Column(modifier = modifier) {
+        StatefulCounter(modifier = modifier)
+        WellnessTaskList()
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun WellnessScreenPreview() {
-    WellnessScreen()
+fun WellnessScreenPreview(modifier: Modifier = Modifier) {
+
 }
