36fdaa81c00b4fd4e8cf88fe5d9202256d289172
Tan N. Truong <tntan92@gmail.com>
2023-03-15 20:40:13 +0900

    Add primary colors

---
+++ theme-codelab/app/src/main/java/com/codelab/theming/ui/start/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.ui.graphics.Color
+
+val red700 = Color(0xffdd0d3c)
+val red800 = Color(0xffd00036)
+val red900 = Color(0xffc20029)
