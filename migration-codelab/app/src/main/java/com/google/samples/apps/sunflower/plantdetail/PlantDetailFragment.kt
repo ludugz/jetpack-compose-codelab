e5bc034bd737c0428adcb1abcd569289875b6453
Tan N. Truong <tntan92@gmail.com>
2023-03-18 21:47:44 +0900

    Init Compose component for Plant Detail fragment

--- migration-codelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
+++ migration-codelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
@@ -21,6 +21,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material.MaterialTheme
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.app.ShareCompat
 import androidx.core.widget.NestedScrollView
@@ -58,6 +59,11 @@ class PlantDetailFragment : Fragment() {
         ).apply {
             viewModel = plantDetailViewModel
             lifecycleOwner = viewLifecycleOwner
+            composeView.setContent {
+                MaterialTheme {
+                    PlantDetailDescription()
+                }
+            }
             callback = object : Callback {
                 override fun add(plant: Plant?) {
                     plant?.let {
