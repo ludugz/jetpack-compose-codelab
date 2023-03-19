fb2165f91e3c58c0bf270c36859870c63c529e53
Tan N. Truong <tntan92@gmail.com>
2023-03-19 12:07:58 +0900

    Create Compose component for Plant Detail title

--- migration-codelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ migration-codelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,13 +16,31 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun PlantDetailDescription() {
+fun PlantDetailDescription(text: String) {
     Surface {
-        Text("Hello Compose")
+        Text(
+            text = text,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally),
+            style = MaterialTheme.typography.h5
+        )
     }
+}
+
+@Preview
+@Composable
+fun PreviewPlantDetailDescription() {
+    PlantDetailDescription("Dummy Text")
 }
--- migration-codelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
+++ migration-codelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
@@ -61,7 +61,7 @@ class PlantDetailFragment : Fragment() {
             lifecycleOwner = viewLifecycleOwner
             composeView.setContent {
                 MaterialTheme {
-                    PlantDetailDescription()
+                    PlantDetailDescription("Sample text")
                 }
             }
             callback = object : Callback {
