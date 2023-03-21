6bdd8e9b0476eaaa6cc56211b5bdf6718e035c79
Tan N. Truong <tntan92@gmail.com>
2023-03-21 16:15:05 +0900

    Update Plant Watering component with padding & alignment

--- migration-codelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantWatering.kt
+++ migration-codelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantWatering.kt
@@ -2,6 +2,7 @@ package com.google.samples.apps.sunflower.plantdetail
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -9,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -19,21 +21,29 @@ import com.google.samples.apps.sunflower.R
 @Composable
 fun PlantWatering(modifier: Modifier = Modifier, wateringInterval: Int) {
     Column(modifier = modifier) {
+        val centerWithPaddingModifier =
+            Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(horizontal = dimensionResource(id = R.dimen.margin_small))
+                .align(alignment = Alignment.CenterHorizontally)
+
+        val normalPadding = dimensionResource(id = R.dimen.margin_normal)
+
         val wateringIntervalText = pluralStringResource(
             R.plurals.watering_needs_suffix, wateringInterval, wateringInterval
         )
+
         Text(
-            modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentWidth(align = Alignment.CenterHorizontally),
+            modifier = centerWithPaddingModifier
+                .padding(top = normalPadding),
             text = stringResource(id = R.string.watering_needs_prefix),
             fontWeight = FontWeight.Bold,
             color = MaterialTheme.colors.primaryVariant
         )
         Text(
-            modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentWidth(align = Alignment.CenterHorizontally),
+            modifier = centerWithPaddingModifier
+                .padding(bottom = normalPadding),
             text = wateringIntervalText,
         )
     }
