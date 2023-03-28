13668743a3efd28e2e6dc4e3d0e33eb547b56c4d
Tan N. Truong <tntan92@gmail.com>
2023-03-28 19:53:30 +0900

    Add visibility animation

--- animation-codelab/start/src/main/java/com/example/android/codelab/animation/ui/home/Home.kt
+++ animation-codelab/start/src/main/java/com/example/android/codelab/animation/ui/home/Home.kt
@@ -230,8 +230,8 @@ private fun HomeFloatingActionButton(
                 contentDescription = null
             )
             // Toggle the visibility of the content with animation.
-            // TODO 2-1: Animate this visibility change.
-            if (extended) {
+            // TODO 2-1: Animate this visibility change. (DONE)
+            AnimatedVisibility (extended) {
                 Text(
                     text = stringResource(R.string.edit),
                     modifier = Modifier
