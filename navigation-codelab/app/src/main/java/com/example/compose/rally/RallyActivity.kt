54297c9d5d6314358932fc4f411ff26b443d2bb4
Tan N. Truong <tntan92@gmail.com>
2023-05-03 22:22:49 +0900

    Add navController & NavHost, handle routing for 3 screens

--- navigation-codelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ navigation-codelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
@@ -28,6 +29,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.compose.rally.ui.components.RallyTabRow
 import com.example.compose.rally.ui.theme.RallyTheme
 
@@ -48,6 +52,7 @@ class RallyActivity : ComponentActivity() {
 fun RallyApp() {
     RallyTheme {
         var currentScreen: RallyDestination by remember { mutableStateOf(Overview) }
+        val navController = rememberNavController()
         Scaffold(
             topBar = {
                 RallyTabRow(
@@ -57,8 +62,28 @@ fun RallyApp() {
                 )
             }
         ) { innerPadding ->
-            Box(Modifier.padding(innerPadding)) {
-                currentScreen.screen()
+            NavHost(
+                navController = navController,
+                startDestination = Overview.route,
+                modifier = Modifier.fillMaxSize(),
+            ) {
+                composable(route = Overview.route) {
+                    Box(Modifier.padding(innerPadding)) {
+                        Overview.screen
+                    }
+                }
+
+                composable(route = Accounts.route) {
+                    Box(Modifier.padding(innerPadding)) {
+                        Accounts.screen
+                    }
+                }
+
+                composable(route = Bills.route) {
+                    Box(Modifier.padding(innerPadding)) {
+                        Bills.screen
+                    }
+                }
             }
         }
     }
