d6c419d2629d9928af4f64c12366eca8c3a41e8b
Tan N. Truong <tntan92@gmail.com>
2023-05-22 23:16:56 +0900

    Init android test for Navigation

---
+++ navigation-codelab/app/src/androidTest/java/com/example/compose/rally/NavigationTest.kt
@@ -0,0 +1,15 @@
+package com.example.compose.rally
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+
+
+/**
+ * Created by Tan N. Truong, on 22 May, 2023
+ * Email: ludugz@gmail.com
+ */
+class NavigationTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+}
