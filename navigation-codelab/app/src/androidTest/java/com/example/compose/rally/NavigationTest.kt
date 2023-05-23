081b6dbf2ad51d093df895fe237ed666d5afd477
Tan N. Truong <tntan92@gmail.com>
2023-05-23 12:58:54 +0900

    Update composable test

--- navigation-codelab/app/src/androidTest/java/com/example/compose/rally/NavigationTest.kt
+++ navigation-codelab/app/src/androidTest/java/com/example/compose/rally/NavigationTest.kt
@@ -1,7 +1,15 @@
 package com.example.compose.rally
 
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import com.example.compose.rally.ui.components.RallyNavHost
+import junit.framework.TestCase.fail
 import org.junit.Rule
+import org.junit.Test
 
 
 /**
@@ -12,4 +20,22 @@ class NavigationTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
+
+    lateinit var navController: TestNavHostController
+
+    @Test
+    fun rallyNavHost_verifyOverviewStartDestination() {
+        composeTestRule.setContent {
+            navController =
+                TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(
+                ComposeNavigator()
+            )
+            RallyNavHost(navController = navController)
+        }
+
+        composeTestRule
+            .onNodeWithContentDescription("Overview Screen")
+            .assertIsDisplayed()
+    }
 }
